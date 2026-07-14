8d40c31fce0fd2f21f47e97aa91c0776e4ad2f3c
SPMSPI <kislicin.artur@gmail.com>
2026-07-14 05:50:45 +0300

    creater and read models.py and views.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    content = models.TextField(max_length=500)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    tags = models.ManyToManyField(
+        Tag,
+        related_name="teg_task",
+        blank=True
+    )
+    def __str__(self):
+        return self.name
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from catalog import models
+from catalog.models import Task
+from django.views import generic, View
+from django.views.generic import DetailView, CreateView, UpdateView, ListView
 
-# Create your views here.
+
+
+def index(request):
+    num_tasks = models.Task.objects.all().count()
+    active_tasks = models.Task.objects.filter(is_completed=False).count()
+    complete_tasks = models.Task.objects.filter(is_completed=True).count()
+    context = {
+        "num_tasks": num_tasks,
+        "active_tasks": active_tasks,
+        "complete_tasks": complete_tasks,
+    }
+    return render(request, "catalog/index.html", context=context)
+
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "tasks"
+    template_name = "catalog/task_list.html"
+
