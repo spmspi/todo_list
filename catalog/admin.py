350fe97b378a8be609f00e9c3cf9f632fc74a331
SPMSPI <kislicin.artur@gmail.com>
2026-07-15 19:49:25 +0300

    Register models in admin.py and delete test.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Task, Tag
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "content", "deadline")
+    list_filter = ("deadline",)
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    list_filter = ("name",)
